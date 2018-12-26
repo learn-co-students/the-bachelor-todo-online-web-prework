930cc883fed20dcbf7305cb4b9d614cb643092c3
Victoria B <viktorija.bourassa@gmail.com>
2018-12-26 20:56:45 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+
+
+def get_first_name_of_season_winner(data, season_number)
+  data[season_number].each do |elem|
+    if elem["status"] == "Winner"
+      name = elem["name"].split(" ")
+      return name[0]
+    end
+    end
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+  count = 0
+   data.each do |season, array|
+     array.each do |obj|
+       obj.each do |k,v|
+         if v == hometown
+           count += 1
+         end
+       end
+     end
+   end
+ count
+  end
 
 def get_occupation(data, hometown)
-  # code here
+  contestants = []
+  data.each do |season, array|
+    array.each do |obj|
+      obj.each do |k,v|
+        if v == hometown
+          return obj["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+  ages = []
+  data[season].each do |elem|
+    age = elem["age"].to_i
+    ages << age
+    end
+    (ages.inject {|sum, el| sum + el}.to_f/ages.size).round
+    end
