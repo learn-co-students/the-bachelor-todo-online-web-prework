a88631330a93a88400e31690b15e7241bb83dabf
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-02 20:06:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,12 @@
 require'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |k,data_hash|
-    data_hash.each  do   |data,attributes|
-      if attributes[status]== "winner"
-        return attributes[name]
+    season=k
+    data_hash.each  do   |data|
+        data.each   {|contest |    }
+        binding.pry
+      if data.include?("winner")
+        return data[name]
       end
     end
   end
