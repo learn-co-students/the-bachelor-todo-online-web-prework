1a4cac87281c867c5b1872b49eddd31bd415027d
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 00:20:58 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,19 +9,52 @@ def get_first_name_of_season_winner(data, season)
     end
   end
 end
-
 def get_contestant_name(data, occupation)
-  # code here
-end
+  data.each do |json,season|
+    season.select do |person_data|
+      if  person_data["occupation"] == occupation
+return person_data ["name"]
 
+      end
+    end
+  end
+end
 def count_contestants_by_hometown(data, hometown)
-  # code here
+i=0
+  data.each do |json,season|
+    season.select do |person_data|
+      if person_data["hometown"]== hometown
+            i+=1
+
+      end
+    end
+  end
+  return i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |json,season|
+    season.select do |person_data|
+      if person_data["hometown"]== hometown
+        return  person_data["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+Array = []
+  data[season].select do |person_data|
+answer= person_data["age"].to_f
+array <<answer
+
+array.average
+return array
 end
+
+end
+
+
+
+
+  # code here
