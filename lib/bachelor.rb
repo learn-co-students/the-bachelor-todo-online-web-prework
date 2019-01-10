3ab0534ecc57fe0165cb8727a107ad711f32dd60
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 20:31:09 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  temp_name = ""
+  data.each do |year, contestant_info|
+    if year == season
+      contestant_info.each do |contestant|
+        contestant.each do |stat, value|
+          if stat == "name"
+            temp_name = value
+          elsif stat == "status" && value == "Winner"
+            return temp_name.split[0]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  temp_name = ""
+  data.each do |year, contestant_info|
+    contestant_info.each do |contestant|
+      contestant.each do |stat, value|
+        if stat == "name"
+          temp_name = value
+        elsif stat == "occupation" && value == occupation
+          return temp_name
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |year, contestant_info|
+    contestant_info.each do |contestant|
+      contestant.each do |stat, value|
+        if stat == "hometown" && value == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |year, contestant_info|
+    contestant_info.each do |contestant|
+      contestant.each do |stat, value|
+        if stat == "hometown" && value != hometown
+          break
+        elsif stat == "occupation"
+          return value
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_years = 0
+  contestant_count = 0
+  data.each do |year, contestant_info|
+    if year == season
+      contestant_info.each do |contestant|
+        contestant.each do |stat, value|
+          if stat == "age"
+            total_years = total_years + value.to_i
+            contestant_count += 1
+          end
+        end
+      end
+    end
+  end
+  puts total_years
+  puts contestant_count
+  return ((total_years/contestant_count.to_f).round)
 end
