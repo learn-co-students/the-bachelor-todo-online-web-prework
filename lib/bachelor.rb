8be317cc400414dffcdd802f828e80e779175676
A Glazer <avigayilglazer@gmail.com>
2019-01-16 13:25:52 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,11 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #data is everything, season is the season number
+  data.each do |season_num, info|
+    # season_num is the season 30, info is all the season info
+  binding.pry
+end
 end
 
 def get_contestant_name(data, occupation)
