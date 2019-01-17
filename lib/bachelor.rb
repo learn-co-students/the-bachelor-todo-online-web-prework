ec733d286a6a1877945e5551101a57d805da9a2f
A Glazer <avigayilglazer@gmail.com>
2019-01-17 19:49:51 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,20 +2,23 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   #data is everything, season is the season number
-  first_name = ""
 
   data.map do |season_num, info|
     # season_num is the season 30, info is all the season info
-  when season == season_num
+
+if season == season_num
+    info.map do |hash|
+    if hash["status"] == "Winner"
+      first_name = hash["name"]
+       first_name.split(" ").first
 
 
 
-
-binding.pry
-
-
+    end
+  end
 end
   end
+
 end
 
 def get_contestant_name(data, occupation)
