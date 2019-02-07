69ab01addc66e3ce7754e713ec95081c5d039725
Christoph Gruener <christophgruener@me.com>
2019-02-07 19:53:47 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_winner = ""
+  data.each do |season_number, contestants_ary|
+    if season_number == season
+      contestants_ary.each do |contestant_hsh|
+        contestant_hsh.each do |stats, value|
+          if value == "Winner"
+            season_winner = contestant_hsh["name"]
+            
+          end
+        end
+      end
+    end
+  end
+  season_winner.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
