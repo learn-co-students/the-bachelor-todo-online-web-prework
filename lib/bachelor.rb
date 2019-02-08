5084b9b0ab2a5ac4bb2684d63d780fd5b6b738b8
Lu Daley <daleyluis@gmail.com>
2019-02-08 21:25:14 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,77 +1,70 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
-  #data is everything, season is the season number
-  final = ""
-  data.map do |season_num, info|
-    # season_num is the season 30, info is all the season info
-
-if season == season_num
-    info.map do |hash|
-    if hash["status"] == "Winner"
-      first_name = hash["name"]
-       final << first_name.split(" ").first
-
-#       binding.pry
+#binding.pry
+    data[season].each do |contestants|
+#binding.pry
+        contestants.each do |key,value|
+#binding.pry
+          if value == "Winner"
+            winner_name = contestants["name"]
+# binding.pry
+             first_name = winner_name.split(' ')
+# binding.pry
+             return first_name.first
+# binding.pry
+        end
+      end
     end
-
-  end
-end
-  end
-final
 end
 
 def get_contestant_name(data, occupation)
-  data.map do |season_num, info|
-    # season_num is the season 30, info is all the season info
+  data.map do |season,info|
     info.map do |hash|
       if hash["occupation"] == occupation
-        return hash["name"]
+      return hash["name"]
       end
     end
   end
 end
 
-
-
-def count_contestants_by_hometown(data, hometown)
-  num_of_ppl = 0
-  data.map do |season_num, info|
-    # season_num is the season 30, info is all the season info
-    info.map do |hash|
-      if hash["hometown"] == hometown
-        num_of_ppl += 1
+def count_contestants_by_hometown(data,city)
+#binding.pry
+  counter = 0
+    data.map do |season,info|
+      info.map do |hash|
+        if hash["hometown"] == city
+        counter+=1
+        end
       end
     end
-  end
-  num_of_ppl
+ counter
 end
 
-def get_occupation(data, hometown)
-  data.map do |season_num, info|
-    # season_num is the season 30, info is all the season info
+def get_occupation(data,city)
+  data.map do |season,info|
     info.map do |hash|
-      if hash["hometown"] == hometown
+      if hash["hometown"] == city
         return hash["occupation"]
       end
     end
+    end
+end
+
+def get_average_age_for_season(data,season)
+  total_age = 0
+  current_contestants = 0
+  data[season].each do |contestants|
+    contestants_age = contestants["age"]
+    total_age += contestants_age.to_f
+    current_contestants += 1
   end
+    average_age = (total_age/current_contestants).round(0)
 end
 
-def get_average_age_for_season(data, season)
-  age_total = 0
 
 
-  data.map do |season_num, info|
-    # season_num is the season 30, info is all the season info
-    if season == season_num
-     data[season_num].each do |person|
+# Failures:
+#   1) bachelor #get_contestant_name returns 'Carly Waddel' when passed data and the string 'Cruise Ship Singer'
+#      Failure/Error: expect(get_contestant_name(data, "Cruise Ship Singer")).to eq("Carly Waddell")
 
-       age_total += person["age"].to_f
-
-end
-
-      return (age_total /data[season_num].count).round
-
-end
-end
-end
+#HASHED OUT ANSWER BELOW###########
