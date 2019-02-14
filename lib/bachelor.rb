fb63a13f8c216a0b6bde093bd9ea634bf3141baf
Eyal Toledano <eyaltoledano@me.com>
2019-02-14 03:33:16 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.each do |show_season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
