1c6100a55a5e6e03a1ef19c308577b8f1c1822cd
Kelly Sciandra <kellysciandra@icloud.com>
2019-02-21 18:49:34 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil
+  data.each do |hash_season, contestants_array|
+    if hash_season == season
+      contestants_array.each do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"].split
+          winner = winner[0]
+        end
+      end
+    end
+end
+winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.each do |hash_season, contestant_array|
+      contestant_array.each do |contestant|
+       if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    person = []
+  data.each do |hash_season, contestants_array|
+    contestants_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        person << hash_season
+      end
+    end
+  end
+  person.length
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = data[season].collect do |contestant|
+    contestant["age"].to_i
+  end
+
+  summed_ages = contestant_ages.reduce(0) {|sum, age| sum += age}
+  contestant_count =  contestant_ages.count.to_f
+  (summed_ages/contestant_count).round
+
 end
