bbd3e310a5d58cfd7e4a0908c87aed794ed0eec5
Natasha Chernis <natashachernis@gmail.com>
2019-03-10 23:21:54 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,34 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find {|contestant|
+    contestant["status"] == "Winner"}["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.find {|season, contestants|
+    contestants.find {|contestant| contestant["occupation"] == occupation}
+  }[1].find {|contestant| contestant["occupation"] == occupation
+    }["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each {|season, contestants|
+    contestants.each {|contestant|
+      count += 1 if contestant["hometown"] == hometown}
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.find {|season, contestants|
+    contestants.find {|contestant| contestant["hometown"] == hometown}
+  }[1].find {|contestant| contestant["hometown"] == hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ((eval data[season].map {|contestant| contestant["age"].to_f}.join("+")
+  )/data[season].length).round
+
 end
