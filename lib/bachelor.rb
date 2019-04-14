a3c724594acb3316612319611690db4288bc355c
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-14 17:02:17 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,82 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    name = ()
+    data.each do |seasons, contestants|
+      #binding.pry
+      if seasons == season
+        contestants.each do |array|
+          array.each do |cats, stats|
+            #binding.pry
+            if array["status"] == "Winner"
+              name = array["name"]
+              return name = name.split.first
+              #binding.pry
+            end  
+          end 
+        end
+      end
+    end  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ()
+    data.each do |seasons, contestants|
+      #binding.pry
+        contestants.each do |array|
+          array.each do |cats, stats|
+            #binding.pry
+            if array["occupation"] == occupation
+              name = array["name"]
+              return name
+              #binding.pry
+            end  
+          end 
+        end
+    end  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    names = []
+    data.each do |seasons, contestants|
+      #binding.pry
+        contestants.each do |array|
+            #binding.pry
+            if array["hometown"] == hometown
+              names << array["name"]
+              #binding.pry
+            end  
+        end
+    end 
+    #binding.pry
+    return names.size
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |seasons, contestants|
+      #binding.pry
+        contestants.each do |array|
+            #binding.pry
+            if array["hometown"] == hometown
+              return array["occupation"]
+              #binding.pry
+            end  
+        end
+    end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+    data.each do |seasons, contestants|
+      #binding.pry
+      if seasons == season
+        contestants.each do |array|
+            #binding.pry
+              ages << array["age"].to_f
+          end 
+          #binding.pry
+          return (ages.sum/ages.size).round
+      end
+     
+    end  
 end
