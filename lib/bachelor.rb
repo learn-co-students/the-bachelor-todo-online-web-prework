94ddfeb559803f4b2bde60988c90e8c78228fbbd
Huy Do <huyddo40@gmail.com>
2019-06-04 03:49:29 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,30 +16,31 @@ def get_first_name_of_season_winner(data, season)
 						# puts "#{fname[0]}"
 						# binding.pry
 					end
-				end
-	  	end
+	  		end
+			end
 		end
 	end
 	fname[0]
 end
 
 def get_contestant_name(data, occupation)
-	c_name = ""
+	c_name = " "
 	data.each do |season_num,value|
 			# puts "#{value}"
-			value.each do |key| #value is array
-				# puts "#{key}"
-				key.each do |k,v| #key is hash
-					# puts "#{k} #{v}"
-					if v == occupation.to_s
-						puts "#{v}"
-						# puts "#{key[:name]}"
-						c_name = key["name"]
-						# binding.pry
-					end
+		value.each do |key| #value is array
+			# puts "#{key}"
+			key.each do |k,v| #key is hash
+				# puts "#{k} #{v}"
+				if v == occupation.to_s
+					puts "#{v}"
+					# puts "#{key[:name]}"
+					c_name = key["name"]
+					# fname[0]
+					# binding.pry
 				end
 			end
 		end
+	end
 	c_name
 end
 
@@ -68,16 +69,16 @@ def get_occupation(data, hometown)
 	job = []
 	data.each do |season_num,value|
 			# puts "#{value}"
-			value.each do |key| #value is array
-				# puts "#{key}"
-				key.each do |k,v| #key is hash
-					# puts "#{k} #{v}"
-					if v == hometown
-							# job << key[:occupation]
-              job << key["occupation"]
-					end
+		value.each do |key| #value is array
+			# puts "#{key}"
+			key.each do |k,v| #key is hash
+				# puts "#{k} #{v}"
+				if v == hometown
+						# job << key[:occupation]
+            job << key["occupation"]
 				end
 			end
+		end
 	end
 	job[0]
 end
@@ -87,25 +88,9 @@ def get_average_age_for_season(data, season)
   count = 0
 	avg_age = 0
 	sum = 0
-	data.each do |season_num,value|
-	  if season == season_num.to_s
-		  # puts "#{season_num}: #{value}"
-		 	value.each do |key|  #value is array
-			#  puts "#{key}"
-				key.each do |k,v|  #key is hash
-					# puts "#{k}: #{v}"
-					# if k == :age
-          if k == :age
-            binding.pry
-						sum += v.to_f
-						count += 1
-						avg_age = sum/count
-						puts "age: #{v.to_i} sum: #{sum} avg_age: #{(sum/count)}"
-						# puts "#{key[:age]}"
-					end
-				end
-	  	end
-		end
+	data[season].each do |person|
+		sum += person["age"].to_i
+		count += 1
 	end
-	avg_age
+	ave_age = (sum/count.to_f).round
 end
