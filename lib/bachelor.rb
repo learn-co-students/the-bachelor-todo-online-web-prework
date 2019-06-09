dca3356006b815c2faa90600588ba7ef0caaf3d2
Nick Baughman <nbaugh1@gmail.com>
2019-06-09 21:39:28 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,28 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num, info|
+    if season_num == season
+      info.each do |attributes, personal_data|
+        if attributes["status"] == "Winner"
+          return attributes["name"].split.first
+        end
+      end
+    end
+  end
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each do |season, info|
+    info.each do |contestants|
+      if contestants["occupation"] == occupation
+        return contestants["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
