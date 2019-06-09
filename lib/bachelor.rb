78c7de6ff0011c162148c4cc907d39cc99a8e447
Nick Baughman <nbaugh1@gmail.com>
2019-06-09 22:18:42 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |season_num, info|
     if season_num == season
-      info.each do |attributes, personal_data|
+      info.each do |attributes|
         if attributes["status"] == "Winner"
           return attributes["name"].split.first
         end
@@ -12,10 +12,7 @@ def get_first_name_of_season_winner(data, season)
   end
 end
 
-
-
 def get_contestant_name(data, occupation)
-
   data.each do |season, info|
     info.each do |contestants|
       if contestants["occupation"] == occupation
@@ -26,13 +23,39 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, info|
+    info.each do |contestants|
+      if contestants["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |contestants|
+      if contestants["hometown"] == hometown
+        return contestants["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  number_contestants = 0
+  average_age = 0
+  data.each do |season_num, info|
+    if season_num == season
+      info.each do |attributes|
+        age_total = age_total + attributes["age"].to_f
+        number_contestants = number_contestants + 1
+      end
+    end
+  end
+  average_age = (age_total / number_contestants).round
 end
